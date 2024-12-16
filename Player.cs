6f62150bd40fbf1a184eaa80778ef12af635b1e5
Matthew John Dunne <mdunne@dunnellc.com>
2024-12-15 23:29:35 -0500

    Seperate out classes

---
+++ Player.cs
@@ -0,0 +1,35 @@
+﻿using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaywrightNFL
+{
+    public class Player
+    {
+        public string? Name { get; set; }
+        public string? Team { get; set; }
+        public string? Position { get; set; }
+    }
+
+    public sealed class QBMap : ClassMap<QB>
+    {
+        public QBMap()
+        {
+            Map(m => m.Name).Index(0);
+            Map(m => m.Position).Index(1);
+            Map(m => m.Team).Index(2);
+            Map(m => m.Completions).Index(3);
+            Map(m => m.Attempts).Index(4);
+            Map(m => m.Yards).Index(5);
+            Map(m => m.Touchdowns).Index(6);
+            Map(m => m.Interceptions).Index(7);
+            Map(m => m.Sacks).Index(8);
+            Map(m => m.Fumbles).Index(9);
+            Map(m => m.FirstDowns).Index(10);
+            Map(m => m.QBRating).Index(11);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,46 +9,11 @@ using System.Threading;
 using System.Globalization;
 using System.Security.Cryptography.X509Certificates;
 using CsvHelper.Configuration;
+
+namespace PlaywrightNFL;
 class Program
 {
-  public class Player
-    {
-        public string? Name { get; set; }
-        public string? Team { get; set; }
-        public string? Position { get; set; }
-    }
-
-   public class QB : Player
-    {
-        public string? Completions { get; set; }
-        public string? Attempts { get; set; }
-        public string? Yards { get; set; }
-        public string? Touchdowns { get; set; }
-        public string? Interceptions { get; set; }
-        public string? Sacks { get; set; }
-        public string? Fumbles { get; set; }
-        public string? FirstDowns { get; set; }
-        public string? QBRating { get; set; }
-    }
-
-    public sealed class QBMap : ClassMap<QB>
-    {
-        public QBMap()
-        {
-            Map(m => m.Name).Index(0);
-            Map(m => m.Position).Index(1);
-            Map(m => m.Team).Index(2);
-            Map(m => m.Completions).Index(3);
-            Map(m => m.Attempts).Index(4);
-            Map(m => m.Yards).Index(5);
-            Map(m => m.Touchdowns).Index(6);
-            Map(m => m.Interceptions).Index(7);
-            Map(m => m.Sacks).Index(8);
-            Map(m => m.Fumbles).Index(9);
-            Map(m => m.FirstDowns).Index(10);
-            Map(m => m.QBRating).Index(11);
-        }
-    }
+   
     static async Task Main()
     {
 
